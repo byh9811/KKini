536cb88240319999bf2b37acbcac05979eef754e
배용현 <byh9811@naver.com>
2023-07-26 17:26:57 +0900

    feat: 레시피 등록 요청 DTO 생성

---
+++ backend/src/main/java/com/kkini/core/domain/recipe/dto/request/RecipeRegisterRequestDto.java
@@ -0,0 +1,32 @@
+package com.kkini.core.domain.recipe.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@Schema(description = "레시피 등록 필드")
+public class RecipeRegisterRequestDto {
+    @Schema(description = "카테고리 ID")
+    private Long categoryId;
+
+    @Schema(description = "레시피 이름")
+    private String name;
+
+    @Schema(description = "소요시간")
+    private int time;
+
+    @Schema(description = "난이도")
+    private int difficulty;
+
+    @Schema(description = "재료")
+    private String ingredient;
+
+    @Schema(description = "대표 이미지")
+    private String image;
+
+    @Schema(description = "조리 과정")
+    private List<String> steps;
+
+}
