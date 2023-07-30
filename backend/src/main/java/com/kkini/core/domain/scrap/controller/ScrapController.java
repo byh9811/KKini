00ffe14c6b87998172249781732de906f5910e13
진병욱 <quddnr678@naver.com>
2023-07-30 21:59:12 +0900

    feat: scrap 엔티티 설계 및 구현

--- backend/src/main/java/com/kkini/core/domain/scrap/controller/ScrapController.java
+++ backend/src/main/java/com/kkini/core/domain/scrap/controller/ScrapController.java
@@ -20,24 +20,25 @@ public class ScrapController {
 
     @Operation(summary = "스크랩 추가", description = "해당 포스트(postId)를 스크랩에 추가합니다.")
     @PostMapping
-    public Response<?> addScrap(@RequestBody AddScrapRequestDto addScrapRequestDto){
+    public Response<?> addScrap(@RequestBody AddScrapRequestDto addScrapRequestDto) {
         log.debug("스크랩 추가");
-        log.debug("{}",addScrapRequestDto);
+        log.debug("{}", addScrapRequestDto);
 
         return Response.OK("OK");
     }
 
     @Operation(summary = "스크랩 삭제", description = "해당 포스트(postId)를 스크랩에서 삭제합니다.")
     @DeleteMapping
-    public Response<?> deleteScrap(@RequestBody DeleteScrapRequestDto deleteScrapRequestDto){
+    public Response<?> deleteScrap(@RequestBody DeleteScrapRequestDto deleteScrapRequestDto) {
         log.debug("스크랩 삭제");
         log.debug("{}", deleteScrapRequestDto);
 
         return Response.OK("OK");
     }
+
     @Operation(summary = "스크랩 리스트", description = "회원(memberId)의 스크랩 리스트를 응답합니다.")
     @GetMapping("/{memberId}")
-    public Response<?> scrapList(@PathVariable String memberId){
+    public Response<?> scrapList(@PathVariable String memberId) {
         log.debug("스크랩 리스트");
         log.debug(memberId);
         List<ScrapListResponseDto> list;
