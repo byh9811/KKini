c7e7f52faf6786bbced63e994307f27b09720f2d
배용현 <byh9811@naver.com>
2023-08-01 11:52:04 +0900

    chore: collection swagger 정리

--- backend/src/main/java/com/kkini/core/domain/collection/controller/CollectionController.java
+++ backend/src/main/java/com/kkini/core/domain/collection/controller/CollectionController.java
@@ -3,6 +3,8 @@ package com.kkini.core.domain.collection.controller;
 import com.kkini.core.domain.collection.dto.response.CollectionListResponseDto;
 import com.kkini.core.global.response.Response;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,12 +19,15 @@ import static com.kkini.core.global.response.Response.OK;
 @RequiredArgsConstructor
 @RequestMapping("/collection")
 @Slf4j
-@Tag(name = "Collection", description = "Collection API Document")
+@Tag(name = "Collection", description = "도감 관리 API")
 public class CollectionController {
 
-    @Operation(summary = "내 도감 리스트 조회", description = "내 도감 리스트를 조회하는 API입니다. 피그마가 없어서 일단 도감ID와 이미지만 보내게 구현해놨습니다.")
+    @Operation(summary = "내 도감 리스트 조회", description = "내 도감 리스트를 조회하는 API입니다.")
+    @Parameters({
+            @Parameter(name = "difficulty", description = "난이도 정보")
+    })
     @GetMapping
-    public Response<List<CollectionListResponseDto>> getMyCollectionList(@RequestParam int difficulty) {
+    public Response<List<CollectionListResponseDto>> getMyCollectionList(@RequestParam(required = false) int difficulty) {
         List<CollectionListResponseDto> list = new ArrayList<>();
         list.add(new CollectionListResponseDto());
         list.add(new CollectionListResponseDto());
