116c30a3f5625e32d6da91e23965fa9bd28010eb
김범창 <tigerspear.kim@gmail.com>
2023-08-01 16:50:13 +0900

    feat: EvaluationController 추가

---
+++ backend/src/main/java/com/kkini/core/domain/evaluation/controller/EvaluationController.java
@@ -0,0 +1,35 @@
+package com.kkini.core.domain.evaluation.controller;
+
+import com.kkini.core.global.response.Response;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import static com.kkini.core.global.response.Response.OK;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/evaluation")
+@Slf4j
+@Tag(name = "Evaluation", description = "포스트 음식 평가 API")
+public class EvaluationController {
+
+    // 수정
+    @Operation(summary = "포스트 음식 평가", description = "포스트 음식의 가치를 금액으로 평가한다.")
+    @Parameters({
+            @Parameter(name = "id", description = "포스트 식별자"),
+            @Parameter(name = "price", description = "금액")
+    })
+    @PutMapping("/{id}")
+    public Response<Void> modifyEvaluation(@PathVariable("id") Long id, @RequestBody int price) {
+        log.debug("modifyEvaluation() Entered");
+        log.debug("{}", id);
+        log.debug("{}", price);
+        return OK(null);
+    }
+
+}
