54d4da27b2ddc5a2804a6f6de682934eb1db590a
김승영 <very7292@naver.com>
2023-08-02 17:42:30 +0900

    refactor: 가운데정렬 수정

--- frontend/kkini/src/routes/N5.jsx
+++ frontend/kkini/src/routes/N5.jsx
@@ -4,18 +4,105 @@ import "./N5.css";
 function N5() {
   return (
     <div className="div-css">
-      <header className="header">
-        <img className="link-SVG" alt="Link SVG" src="link-SVG.svg" />
-        <div className="div-css-mmlq">
-          <img className="img" alt="Link" src="link.svg" />
-          <div className="div-css-oegnej">
-            <div className="div-css-pgrxd">
-              <div className="div-css-rdto" />
-              <div className="div-css-ilb" />
+      <div className="screen">
+        <div className="overlap-wrapper">
+          <div className="overlap">
+            <div className="group">
+              <img className="img" alt="Group" src="group.png" />
+              <img className="group-2" alt="Group" src="image.png" />
+              <div className="link">
+                <div className="overlap-group">
+                  <img className="SVG" alt="Svg" src="SVG.svg" />
+                </div>
+              </div>
+              <div className="overlap-group-wrapper">
+                <div className="SVG-wrapper">
+                  <img className="SVG" alt="Svg" src="image.svg" />
+                </div>
+              </div>
+              <div className="div">
+                <div className="img-wrapper">
+                  <img className="SVG" alt="Svg" src="SVG-2.svg" />
+                </div>
+              </div>
+              <div className="div-wrapper">
+                <div className="overlap-2">
+                  <img className="element" alt="Element" src="168765237912753226-jpeg.png" />
+                  <div className="div-css">
+                    <h1 className="text-wrapper">+48</h1>
+                  </div>
+                </div>
+              </div>
+              <div className="frame">
+                <div className="heading">
+                  <div className="text-wrapper-2">사진</div>
+                  <div className="text-wrapper-3">55</div>
+                </div>
+                <div className="div-css-vxghs">
+                  <div className="link-2">전체보기</div>
+                </div>
+              </div>
+              <div className="frame-2">
+                <div className="heading">
+                  <div className="text-wrapper-2">레시피</div>
+                  <div className="text-wrapper-3">5</div>
+                </div>
+                <div className="div-css-vxghs">
+                  <div className="link-2">전체보기</div>
+                </div>
+              </div>
+              <div className="div-css-jvf">
+                <div className="link-3">
+                  <div className="overlap-group-2">
+                    <img className="SVG" alt="Svg" src="SVG-3.svg" />
+                  </div>
+                </div>
+              </div>
+              <div className="link-wrapper">
+                <div className="link-3">
+                  <div className="overlap-group-3">
+                    <img className="SVG" alt="Svg" src="SVG-4.svg" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="div-css-wrapper">
+              <div className="div-css-oycmd">
+                <div className="descriptions">
+                  <div className="term">팔로워</div>
+                  <div className="details">
+                    <div className="link-4">4</div>
+                    <div className="pseudo" />
+                  </div>
+                  <div className="term-2">팔로잉</div>
+                  <div className="details-link">0</div>
+                </div>
+              </div>
+            </div>
+            <div className="frame-wrapper">
+              <div className="frame-3">
+                <div className="text-wrapper-4">승영쿡</div>
+                <img className="tvdd" alt="Tvdd" src="tvdd-2.png" />
+              </div>
+            </div>
+            <div className="group-5">
+              <div className="overlap-4">
+                <div className="rectangle" />
+                <div className="rectangle-2" />
+                <div className="text-wrapper-5">13/30</div>
+                <img className="image-3" alt="Image" src="image-9.png" />
+              </div>
+            </div>
+            <div className="div-css-uebeg">
+              <div className="overlap-5" />
+            </div>
+            <img className="removebg-preview" alt="Removebg preview" src="removebg-preview-2.png" />
+            <div className="button">
+              <div className="text-wrapper-6">팔로우</div>
             </div>
           </div>
         </div>
-      </header>
+      </div>
     </div>
 
 
