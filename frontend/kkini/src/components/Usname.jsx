33aa61de03f33fc458f3acac2bd2da5d758f4ce9
이승태 <bs007bsk@naver.com>
2023-08-04 16:40:48 +0900

    feat: 검색창구현1

---
+++ frontend/kkini/src/components/Usname.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Usname(props) {
+  // 여기에 사용자 이름 검색 결과를 표시하는 로직을 구현하세요.
+
+  return (
+    <div>
+      <h2>사용자 이름 검색 결과</h2>
+      {/* 여기에 사용자 이름 검색 결과를 표시하세요 */}
+    </div>
+  );
+}
+
+export default Usname;
--- frontend/kkini/src/routes/N2.jsx
+++ frontend/kkini/src/routes/N2.jsx
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import RealSearch from './../components/RealSearch';
+import SearchBar from '../components/SearchBar';
+import Usname from '../components/Usname';
+import Feedname from '../components/Feedname';
 
 function N2() {
+  let [탭, 탭변경] = useState(0);
+
+  const handleSearchQuery = (query) => {
+    // 여기에 검색 로직을 구현하고, 결과를 상태에 저장합니다.
+    console.log("검색 쿼리:", query);
+    // 예: 검색 결과를 서버로부터 가져오거나 필터링하여 화면에 표시
+  };
+
   return (
     <div>
+      <SearchBar onSearch={handleSearchQuery}></SearchBar>
+      <Navbar className="justify-content-center">
+        <Container>
+          <Nav className="mx-auto" defaultActiveKey="link-0">
+            <Nav.Link onClick={() => { 탭변경(0) }} eventKey="link-0">사용자 이름 검색결과</Nav.Link>
+            <Nav.Link onClick={() => { 탭변경(1) }} eventKey="link-1">내용 검색결과</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <TabContent 탭={탭}></TabContent>
       n2입니다아아아
     </div>
   );
+}
+
+function TabContent(props) {
+  if (props.탭 === 0) {
+    return <Usname></Usname>;
+  } else {
+    return <Feedname></Feedname>;
+  }
 }
 
 export default N2;
