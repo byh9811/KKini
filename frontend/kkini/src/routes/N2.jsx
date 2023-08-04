794e75be9e1d769b49c391012c8f0ce25805c64e
이승태 <bs007bsk@naver.com>
2023-08-04 17:51:35 +0900

    feat:검색기능추가2

--- frontend/kkini/src/routes/N2.jsx
+++ frontend/kkini/src/routes/N2.jsx
@@ -1,32 +1,35 @@
 import React, { useState } from 'react';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import RealSearch from './../components/RealSearch';
-import SearchBar from '../components/SearchBar';
 import Usname from '../components/Usname';
 import Feedname from '../components/Feedname';
+import SearchBar from '../components/SearchBar';
 
 function N2() {
   let [탭, 탭변경] = useState(0);
+  let [showResults, setShowResults] = useState(false);
 
-  const handleSearchQuery = (query) => {
-    // 여기에 검색 로직을 구현하고, 결과를 상태에 저장합니다.
-    console.log("검색 쿼리:", query);
-    // 예: 검색 결과를 서버로부터 가져오거나 필터링하여 화면에 표시
+  const handleSearch = (query) => {
+    // 여기서 query를 사용하여 검색을 수행할 수 있습니다.
+    setShowResults(true);
   };
 
   return (
     <div>
-      <SearchBar onSearch={handleSearchQuery}></SearchBar>
-      <Navbar className="justify-content-center">
-        <Container>
-          <Nav className="mx-auto" defaultActiveKey="link-0">
-            <Nav.Link onClick={() => { 탭변경(0) }} eventKey="link-0">사용자 이름 검색결과</Nav.Link>
-            <Nav.Link onClick={() => { 탭변경(1) }} eventKey="link-1">내용 검색결과</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+      <SearchBar onSearch={handleSearch}></SearchBar>
+      {showResults && (
+        <>
+          <Navbar className="justify-content-center">
+            <Container>
+              <Nav className="mx-auto" defaultActiveKey="link-0">
+                <Nav.Link onClick={() => { 탭변경(0) }} eventKey="link-0">사용자 이름 검색결과</Nav.Link>
+                <Nav.Link onClick={() => { 탭변경(1) }} eventKey="link-1">내용 검색결과</Nav.Link>
+              </Nav>
+            </Container>
+          </Navbar>
 
-      <TabContent 탭={탭}></TabContent>
+          <TabContent 탭={탭}></TabContent>
+        </>
+      )}
       n2입니다아아아
     </div>
   );
