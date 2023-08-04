165a08ff0b1dee720916a2daeb7db277083d05c2
박태규 <everlastin13@naver.com>
2023-08-04 16:35:59 +0900

    feat: 이미지 다중 등록, 이미지 서버 전송 기능 구현

--- frontend/kkini/src/components/Upload.jsx
+++ frontend/kkini/src/components/Upload.jsx
@@ -2,115 +2,104 @@ import React, { useEffect, useState } from 'react';
 import "./Upload.css";
 import { Button } from "@mui/material";
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import axios from 'axios';
 
 function Upload() {
-  /*
-    fileList는 아래와 같은 object의 array로 구성
-    {
-      fileObject: files[i],
-      preview_URL: preview_URL,
-      type: fileType,
-    }
-  */
   const [fileList, setFileList] = useState([]);
   let inputRef;
 
-  const saveImage = async (e) => {
+  const saveImage = (e) => {
     e.preventDefault();
-    // state update전 임시로 사용할 array
     const tmpFileList = [];
     const files = e.target.files;
     if (files) {
-      for (let i = 0; i < files.length; i++) {
+      for (let i = 0; i < files.length && fileList.length < 5; i++) {
         const preview_URL = URL.createObjectURL(files[i]);
-        const fileType = files[i].type.split("/")[0];
-        if (fileType === "video") {
-          /*
-            video일 때 createElement 후 video의 시간을 재는 과정이 비동기라서 state update보다 나중에 실행되어
-            정상적으로 state에 반영되지 않기 때문에 promise와 await로 잠시 코드를 멈춰서 비동기를 해결하게 한다.
-          */
-          await new Promise((resolve) => {
-            const videoElement = document.createElement("video");
-            videoElement.src = preview_URL;
-            const timer = setInterval(() => {
-              if (videoElement.readyState === 4) {
-                if (videoElement.duration > 16) {
-                  alert("동영상의 길이가 16초보다 길면 안됩니다");
-                  // src에 넣지 않을 것이므로 미리보기 URL 제거
-                  URL.revokeObjectURL(preview_URL);
-                } else {
-                  fileList.push({
-                    fileObject: files[i],
-                    preview_URL: preview_URL,
-                    type: fileType,
-                  });
-                }
-                clearInterval(timer);
-                // Promise resolve
-                resolve("good");
-              }
-            }, 500);
-          });
-        } else {
-          fileList.push({
-            fileObject: files[i],
-            preview_URL: preview_URL,
-            type: fileType,
-          });
-        }
+        tmpFileList.push({
+          fileObject: files[i],
+          preview_URL: preview_URL,
+        });
       }
     }
-    // 마지막에 state update
     setFileList([...tmpFileList, ...fileList]);
   };
-  // index에 해당하는 state 삭제
+
   const deleteImage = (index) => {
     const tmpFileList = [...fileList];
     tmpFileList.splice(index, 1);
     setFileList(tmpFileList);
   }
 
-  useEffect(()=>{
-    // 컴포넌트가 언마운트되면 revokeObjectURL로 미리보기 URL 모두 삭제
+  const moveImage = (fromIndex, toIndex) => {
+    const tmpFileList = [...fileList];
+    const [removedItem] = tmpFileList.splice(fromIndex, 1);
+    tmpFileList.splice(toIndex, 0, removedItem);
+    setFileList(tmpFileList);
+  }
+
+  useEffect(() => {
     return () => {
-      fileList?.forEach((item)=>{
+      fileList?.forEach((item) => {
         URL.revokeObjectURL(item.preview_URL);
       })
     }
-  }, [])
+  }, []);
 
-  console.log(fileList);
+  const handleFileUpload = () => {
+    const formData = new FormData();
+    for (let i = 0; i < fileList.length; i++) {
+      formData.append("fileType", "post");
+      formData.append("files", fileList[i].fileObject);
+    }
+
+    axios.post('http://localhost:8080/api/s3', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+    .then((response) => {
+      console.log('업로드 성공:', response.data);
+    })
+    .catch((error) => {
+      console.error('업로드 실패:', error);
+    });
+  };
+
   return (
     <div className="uploader-wrapper">
-      <input
-        type="file" multiple={true} accept="video/*, image/*"
-        onChange={saveImage}
-        // 클릭할 때 마다 file input의 value를 초기화 하지 않으면 버그가 발생할 수 있다
-        // 사진 등록을 두개 띄우고 첫번째에 사진을 올리고 지우고 두번째에 같은 사진을 올리면 그 값이 남아있음!
-        onClick={(e) => e.target.value = null}
-        ref={refParam => inputRef = refParam}
-        style={{display: "none"}}
-      />
+      {fileList.length < 5 && (
+        <input
+          type="file" multiple={true} accept="image/*"
+          onChange={saveImage}
+          onClick={(e) => e.target.value = null}
+          ref={refParam => inputRef = refParam}
+          style={{ display: "none" }}
+        />
+      )}
 
       <div className="file-container">
-          {fileList?.map((item, index) => (
-            <div className="file-wrapper">
-              {item.type === "image" ? (
-                <img src={item.preview_URL}/>
-
-              ) : (
-                <video src={item.preview_URL} autoPlay={false} controls={true}/>
-              )}
-              <div className="delete-button" onClick={()=>{deleteImage(index)}}>
-                <HighlightOffIcon fontSize="large" color="error"/>
-              </div>
+        {fileList?.map((item, index) => (
+          <div className="file-wrapper" key={index} draggable onDragStart={(e) => e.dataTransfer.setData("index", index)} onDragOver={(e) => e.preventDefault()} onDrop={(e) => moveImage(e.dataTransfer.getData("index"), index)}>
+            <img src={item.preview_URL} alt="Uploaded file" />
+            <div className="delete-button" onClick={() => { deleteImage(index) }}>
+              <HighlightOffIcon fontSize="large" color="error" />
             </div>
-          ))}
+          </div>
+        ))}
       </div>
 
-      <div className="upload-button">
-        <Button variant="contained" onClick={() => inputRef.click()}>
-          Upload
+      {fileList.length < 5 && (
+        <div className="upload-button">
+          <Button variant="contained" onClick={() => inputRef.click()}>
+            Upload
+          </Button>
+        </div>
+      )}
+
+      {/* Additional Button */}
+      <div className="additional-button">
+        <Button variant="contained" color="primary" onClick={handleFileUpload}>
+          Additional Button
         </Button>
       </div>
     </div>
