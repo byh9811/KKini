2f866395ec6cc8cc49bbeef5061a320656fabd18
진병욱 <quddnr678@naver.com>
2023-08-07 17:51:44 +0900

    chore: 팔로우, 팔로워 리스트에 페이징 구현

--- backend/src/main/java/com/kkini/core/domain/follow/service/FollowQueryService.java
+++ backend/src/main/java/com/kkini/core/domain/follow/service/FollowQueryService.java
@@ -1,15 +1,17 @@
 package com.kkini.core.domain.follow.service;
 
 import com.kkini.core.domain.follow.dto.response.FollowListResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface FollowQueryService {
 
     // 팔로워 리스트
-    List<FollowListResponseDto> getFollowerList(Long id);
+    Page<FollowListResponseDto> getFollowerList(Long id, Pageable pageable);
     // 팔로우 리스트
-    List<FollowListResponseDto> getFollowList(Long id);
+    Page<FollowListResponseDto> getFollowList(Long id, Pageable pageable);
     // 팔로워 수
     int countFollowers(Long id);
     // 팔로우 수
