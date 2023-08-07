192d364ae9ae179ad2caa84828bff3e3c78d83f2
진병욱 <quddnr678@naver.com>
2023-08-07 17:09:40 +0900

    chore: 스크랩 리스트 요청시에 spring security 사용으로 변경

--- backend/src/main/java/com/kkini/core/domain/scrap/controller/ScrapController.java
+++ backend/src/main/java/com/kkini/core/domain/scrap/controller/ScrapController.java
@@ -39,8 +39,8 @@ public class ScrapController {
         log.debug("## 스크랩을 추가합니다.");
         log.debug("추가할 포스트 식별자 : {}", postId);
         AddScrapRequestDto addScrapRequestDto = new AddScrapRequestDto();
-        addScrapRequestDto.setMemberId(1L); // 해당 부분은 추후에 user.getMemberId()로 교체할 예정
-//        addScrapRequestDto.setMemberId(userPrincipal.getId()); // 해당 부분은 추후에 user.getMemberId()로 교체할 예정
+//        addScrapRequestDto.setMemberId(1L); // 해당 부분은 추후에 user.getMemberId()로 교체할 예정
+        addScrapRequestDto.setMemberId(userPrincipal.getId()); // 해당 부분은 추후에 user.getMemberId()로 교체할 예정
         addScrapRequestDto.setPostId(postId);
         scrapService.addScrap(addScrapRequestDto);
 
@@ -60,11 +60,11 @@ public class ScrapController {
 
     @Operation(summary = "스크랩 리스트", description = "회원(memberId)의 스크랩 리스트를 응답합니다.")
     @Parameter(name = "memberId", description = "조회를 원하는 회원 식별자")
-    @GetMapping("/list/{memberId}")
-    public Response<Page<ScrapListResponseDto>> scrapList(@PathVariable long memberId, @PageableDefault(size = 20) Pageable pageable) {
+    @GetMapping("/list}")
+    public Response<Page<ScrapListResponseDto>> scrapList(@Parameter(hidden = true) @AuthenticationPrincipal UserPrincipal userPrincipal, @PageableDefault(size = 20) Pageable pageable) {
         log.debug("## 스크랩 리스트를 조회합니다.");
-        log.debug("조회할 멤버 식별자 : {}",memberId);
-        Page<ScrapListResponseDto> list = scrapQueryService.getScrapList(memberId,pageable);
+        log.debug("조회할 멤버 식별자 : {}",userPrincipal.getId());
+        Page<ScrapListResponseDto> list = scrapQueryService.getScrapList(userPrincipal.getId(), pageable);
 
         return OK(list);
     }
