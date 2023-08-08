7f40956577289a34ce9a8dbf7e6c180e6c54d67f
seungtae1377 <bs007bsk@naver.com>
2023-08-08 23:30:54 +0900

    feat: 좋아요색

--- frontend/kkini/src/routes/N1.jsx
+++ frontend/kkini/src/routes/N1.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import Timeline from '../components/Timeline'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react';
+import Timeline from '../components/Timeline';
+import axios from 'axios';
 
 function N1() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://172.17.0.1:8080/api/post?page=0&size=1&sort=string')
+      .then(response => {
+        if (response.data.success) {
+          setPosts(response.data.response);
+        }
+      })
+      .catch(error => {
+        console.error("Error fetching posts:", error);
+      });
+  }, []);
+
   window.scrollTo(0, 0);
-  
+
   return (
     <div className='homepage'>
       <div>
         <p>Hello, World!</p>
-        <button onClick={()=>{
-          axios.put('http://localhost:8080/api/s3/test')
-          .then((response)=>{
-            console.log(response.data)
-          })
-          .catch(()=>{
-            console.log('실패애애')
-          })
-        }}>버튼</button>
       </div>
-
       <div className='homepage__timeline'>
-        <Timeline />
+        <Timeline posts={posts} />
       </div>
     </div>
-  )
+  );
 }
 
 export default N1;
