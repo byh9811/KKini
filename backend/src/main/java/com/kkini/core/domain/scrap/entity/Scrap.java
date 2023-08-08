8e5da077e2f556c18ed058f164d016ff778491f3
김범창 <tigerspear.kim@gmail.com>
2023-08-08 17:57:28 +0900

    feat: 포스트 삭제 onDelete

--- backend/src/main/java/com/kkini/core/domain/scrap/entity/Scrap.java
+++ backend/src/main/java/com/kkini/core/domain/scrap/entity/Scrap.java
@@ -6,6 +6,8 @@ import com.kkini.core.global.entity.BaseEntityWithCreatedTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
@@ -23,6 +25,7 @@ public class Scrap extends BaseEntityWithCreatedTime {
 
     @ManyToOne
     @JoinColumn(name = "post_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Post post;
 
 }
