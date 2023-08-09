eda8da4c3533e744e9850bb41856f055f5353b45
김범창 <tigerspear.kim@gmail.com>
2023-08-09 15:54:14 +0900

    fix: follow 패키지 복원

--- backend/src/main/java/com/kkini/core/domain/scrap/dto/request/AddScrapRequestDto.java
+++ backend/src/main/java/com/kkini/core/domain/scrap/dto/request/AddScrapRequestDto.java
@@ -8,7 +8,7 @@ import lombok.Data;
 public class AddScrapRequestDto {
 
     @Schema(description = "멤버 식별자")
-    private String email;
+    private Long memberId;
 
     @Schema(description = "포스트 식별자")
     private Long postId;
