2ec748878b9668d54c627b3ec01206961d60ee22
김범창 <tigerspear.kim@gmail.com>
2023-08-10 15:11:09 +0900

    feat: Reaction 생성 기능 구현

--- backend/src/main/java/com/kkini/core/domain/reaction/controller/ReactionController.java
+++ backend/src/main/java/com/kkini/core/domain/reaction/controller/ReactionController.java
@@ -1,5 +1,8 @@
 package com.kkini.core.domain.reaction.controller;
 
+import com.kkini.core.domain.oauth2.UserPrincipal;
+import com.kkini.core.domain.reaction.dto.request.ReactionRegisterRequestDto;
+import com.kkini.core.domain.reaction.service.ReactionService;
 import com.kkini.core.global.response.Response;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -7,6 +10,7 @@ import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import static com.kkini.core.global.response.Response.OK;
@@ -18,18 +22,20 @@ import static com.kkini.core.global.response.Response.OK;
 @Tag(name = "Reaction", description = "Reaction 관리 API")
 public class ReactionController {
 
-    // 수정
+    private final ReactionService reactionService;
+
     @Operation(summary = "포스트 반응", description = "포스트에 반응(좋아요/싫어요/없음)을 한다.")
     @Parameters({
             @Parameter(name = "id", description = "포스트 식별자"),
             @Parameter(name = "state", description = "상태(좋아요/싫어요/없음)")
     })
-    @PutMapping("/{id}")
-    public Response<Void> modifyReaction(@PathVariable("id") Long id, @RequestBody Boolean state) {
-        // 프론트에서 현재상태와 비교&계산하여 어떤 상태(좋아요/싫어요/없음)로 변환할 것인지 계산하여 전달한다.
-        log.debug("modifyReaction() Entered");
-        log.debug("{}", id);
-        log.debug("{}", state);
+    @PostMapping
+    public Response<Void> modifyReaction(
+            @RequestBody ReactionRegisterRequestDto reactionRegisterRequestDto,
+            @Parameter(hidden = true) @AuthenticationPrincipal UserPrincipal userPrincipal
+    ) {
+        reactionService.saveReaction(reactionRegisterRequestDto, userPrincipal.getId());
+
         return OK(null);
     }
 
