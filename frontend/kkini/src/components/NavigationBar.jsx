4ba892d6da2480ef9898e9199ad2fa4e83c6d251
박태규 <everlastin13@naver.com>
2023-08-10 11:16:52 +0900

    feat: 포스트 등록 서버, 클라 연동

--- frontend/kkini/src/components/NavigationBar.jsx
+++ frontend/kkini/src/components/NavigationBar.jsx
@@ -1,5 +1,44 @@
+// import React, { useRef, useState } from 'react';
+// import { useNavigate } from 'react-router-dom'
+// import BottomNavigation from '@mui/material/BottomNavigation';
+// import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+// import RestoreIcon from '@mui/icons-material/Restore';
+// import FavoriteIcon from '@mui/icons-material/Favorite';
+// import ArchiveIcon from '@mui/icons-material/Archive';
+
+// function NavigationBar() {
+
+//   const navigate = useNavigate();
+
+//   const ref = useRef(null);
+//   const [value, setValue] = useState(0);
+
+//   const handleChange = (event, newValue) => {
+//     setValue(newValue);
+//   }
+
+
+//   return (
+//     <BottomNavigation
+//         sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, width: 500, margin: '0 auto' }}
+//         ref={ref}
+//         value={value}
+//         onChange={handleChange}
+//       >
+//         <BottomNavigationAction onClick={() => {navigate('/n1')}} label="Recents" icon={<RestoreIcon />} />
+//         <BottomNavigationAction onClick={() => {navigate('/n2')}} label="Favorites" icon={<FavoriteIcon />} />
+//         <BottomNavigationAction onClick={() => {navigate('/n3')}} label="Recents" icon={<RestoreIcon />} />
+//         <BottomNavigationAction onClick={() => {navigate('/n4')}} label="Archive" icon={<ArchiveIcon />} />
+//         <BottomNavigationAction onClick={() => {navigate('/n5')}} label="Recents" icon={<RestoreIcon />} />
+//       </BottomNavigation>
+
+//   );
+// }
+
+// export default NavigationBar;
+
 import React, { useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import RestoreIcon from '@mui/icons-material/Restore';
@@ -17,7 +56,6 @@ function NavigationBar() {
     setValue(newValue);
   }
 
-
   return (
     <BottomNavigation
         sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, width: 500, margin: '0 auto' }}
@@ -25,13 +63,12 @@ function NavigationBar() {
         value={value}
         onChange={handleChange}
       >
-        <BottomNavigationAction onClick={() => {navigate('/n1')}} label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction onClick={() => {navigate('/n2')}} label="Favorites" icon={<FavoriteIcon />} />
-        <BottomNavigationAction onClick={() => {navigate('/n3')}} label="Recents" icon={<RestoreIcon />} />
-        <BottomNavigationAction onClick={() => {navigate('/n4')}} label="Archive" icon={<ArchiveIcon />} />
-        <BottomNavigationAction onClick={() => {navigate('/n5')}} label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction onClick={() => {navigate('/home/n1')}} label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction onClick={() => {navigate('/home/n2')}} label="Favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction onClick={() => {navigate('/home/n3')}} label="Recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction onClick={() => {navigate('/home/n4')}} label="Archive" icon={<ArchiveIcon />} />
+        <BottomNavigationAction onClick={() => {navigate('/home/n5')}} label="Recents" icon={<RestoreIcon />} />
       </BottomNavigation>
-
   );
 }
 
