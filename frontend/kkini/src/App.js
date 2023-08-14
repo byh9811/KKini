b476bf6e2a0c22d6a5451b1fa618f80ecec343c7
이승태 <bs007bsk@naver.com>
2023-08-14 15:18:26 +0900

    feat:도감css

--- frontend/kkini/src/App.js
+++ frontend/kkini/src/App.js
@@ -13,6 +13,8 @@ import Naver from './routes/login/Naver';
 import Withdrawal from './routes/login/Withdrawal';
 import FollowList from './components/mypage/FollowList';
 
+import Book from './routes/pages/Book';
+
 // App.js
 function App() {
   const [isLogIn, setIsLogIn ] = useState(false);
@@ -35,6 +37,7 @@ function App() {
         <Route path="/naver" element={<Naver/>} />
         <Route path="/withdrawal" element={<Withdrawal />} />
         <Route path="/followlist/:userId?" element={<FollowList />}/>
+        <Route path="/book" element={<Book />} />
         <Route path="/home/*" element={<Home />}>
           <Route path="n1" element={<N1Home />} />
           <Route path="n2" element={<N2Search />} />
