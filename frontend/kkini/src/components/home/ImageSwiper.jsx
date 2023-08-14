001acfa695d3ddd409cca7b404758c808a38d84e
진병욱 <quddnr678@naver.com>
2023-08-14 21:31:28 +0900

    feat: 메인 피드 스와이프 구현

---
+++ frontend/kkini/src/components/home/ImageSwiper.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+
+function ImageSwiper({ postImage }) {
+  return (
+    <Swiper pagination={true} className="mySwiper" style={{ width: "100%", height: "100%" }}>
+      {postImage.map((link, index) => (
+        <SwiperSlide key={index} style={{ alignItems: "center" }}>
+          <img src={link} alt={`Image ${index}`} style={{ width: "50vw", height: "50vh" }} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
+
+export default ImageSwiper;
