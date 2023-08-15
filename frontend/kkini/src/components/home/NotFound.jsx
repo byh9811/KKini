133fd27b6de710f5488b370b38d8e1900d22a3de
진병욱 <quddnr678@naver.com>
2023-08-15 23:59:05 +0900

    fix: 마이페이지 없는 유저 호출시 404로 이동

--- frontend/kkini/src/components/home/NotFound.jsx
+++ frontend/kkini/src/components/home/NotFound.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import { Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-
-const goBack = () => {
-  window.history.back();
-};
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate("/home/feed");
+  };
   return (
     <div>
-      {" "}
       <h1>404 NotFound</h1>
       <h3>해당 페이지는 존재하지 않습니다!!</h3>
       <h3>다른 URL로 접속해주세요~!</h3>
       <Button variant="primary" onClick={goBack}>
         돌아가기
       </Button>
-      <p>계속 해당 페이지가 뜬다면 관리자에게 문의해주세요.</p>
+      <p>계속 해당 페이지가 뜬다면 끼니 관리자에게 문의해주세요.</p>
     </div>
   );
 };
