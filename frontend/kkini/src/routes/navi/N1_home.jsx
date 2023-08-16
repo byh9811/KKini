7e2169df002a1261b987ef9b55fe425f5cbea448
박태규 <everlastin13@naver.com>
2023-08-16 13:37:11 +0900

    feat: N1, Timeline 컴포넌트 통합

--- frontend/kkini/src/routes/navi/N1_home.jsx
+++ frontend/kkini/src/routes/navi/N1_home.jsx
@@ -1,36 +1,66 @@
 import React, { useState, useEffect } from "react";
-import Timeline from "../../components/home/Timeline";
+import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import axios from "axios";
-import Drawer from "../../components/home/Drawer";
+import Post from "../../components/home/Post.jsx";
 
 function N1Home() {
   const [posts, setPosts] = useState([]);
-  const [selectedPostId, setSelectedPostId] = useState(null); // 추가된 상태
 
   useEffect(() => {
-    axios
-      .get("/post", {
-        params: {
-          page: 0,
-        },
-      })
-      .then((response) => {
-        if (response.data.success) {
-          setPosts(response.data.response);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching posts:", error);
-      });
+    axios.get("/post", {
+      params: {
+        page: 0,
+      },
+    })
+    .then((response) => {
+      if (response.data.success) {
+        setPosts(response.data.response.content);
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching posts:", error);
+    });
   }, []);
-  const handlePostSelect = (postId) => {
-    setSelectedPostId(postId);
+
+  const navigate = new useNavigate();
+
+  const goMake = () => {
+    navigate("/home/make");
   };
 
   return (
-    <div>
-      <Timeline posts={posts} onSelectPost={handlePostSelect} />
-      {selectedPostId && <Drawer isOpen={!!selectedPostId} onClose={() => setSelectedPostId(null)} postId={selectedPostId} />}
+    <div className="timeline" style={{ padding: 0 }}>
+      {posts.length > 0 ? (
+        <div className="timeline_posts" style={{ padding: 0 }}>
+          {posts.map((post, index) => (
+            <Post
+              key={index}
+              index={index}
+              avgPrice={post.avgPrice}
+              commentCnt={post.commentCnt}
+              contents={post.contents}
+              createDateTime={post.createDateTime}
+              disLikeCnt={post.disLikeCnt}
+              postId={post.id}
+              postImage={post.imageList}
+              isScrap={post.isScrap}
+              likeCnt={post.likeCnt}
+              user={post.memberName}
+              myPrice={post.myPrice}
+              reaction={post.reaction}
+              recipeId={post.recipeId}
+              recipeName={post.recipeName}
+            />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>등록된 게시글이 없어요</p>
+          <p>게시글을 등록하러 가볼까요 ?</p>
+          <Button onClick={goMake}>등록하기</Button>
+        </div>
+      )}
     </div>
   );
 }
