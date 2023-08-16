6ead9c64c72ee5bf5681a15496dbf2153e3f04a3
배용현 <byh9811@naver.com>
2023-08-16 21:40:51 +0900

    docs: ddl에 cascade 추가

--- exec/DDL.sql
+++ exec/DDL.sql
@@ -1,3 +1,22 @@
+use s09p12c210;
+
+-- drop tables
+
+DROP TABLE IF EXISTS `evaluation`;
+DROP TABLE IF EXISTS `comment`;
+DROP TABLE IF EXISTS `scrap`;
+DROP TABLE IF EXISTS `reaction`;
+DROP TABLE IF EXISTS `post_image`;
+DROP TABLE IF EXISTS `post`;
+DROP TABLE IF EXISTS `collection`;
+DROP TABLE IF EXISTS `recipe`;
+DROP TABLE IF EXISTS `preference`;
+DROP TABLE IF EXISTS `history`;
+DROP TABLE IF EXISTS `follow`;
+DROP TABLE IF EXISTS `member`;
+DROP TABLE IF EXISTS `hibernate_sequence`;
+DROP TABLE IF EXISTS `category`;
+
 -- s09p12c210.category definition
 
 CREATE TABLE `category` (
@@ -47,8 +66,8 @@ CREATE TABLE `follow` (
                           PRIMARY KEY (`id`),
                           KEY `FKd44awit5xp9iv3nbmolc1u8ls` (`me_id`),
                           KEY `FKi4vw44ytov6dhpyir57thbdbh` (`target_id`),
-                          CONSTRAINT `FKd44awit5xp9iv3nbmolc1u8ls` FOREIGN KEY (`me_id`) REFERENCES `member` (`id`),
-                          CONSTRAINT `FKi4vw44ytov6dhpyir57thbdbh` FOREIGN KEY (`target_id`) REFERENCES `member` (`id`)
+                          CONSTRAINT `FKd44awit5xp9iv3nbmolc1u8ls` FOREIGN KEY (`me_id`) REFERENCES `member` (`id`) ON DELETE CASCADE,
+                          CONSTRAINT `FKi4vw44ytov6dhpyir57thbdbh` FOREIGN KEY (`target_id`) REFERENCES `member` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -61,7 +80,7 @@ CREATE TABLE `history` (
                            `member_id` bigint(20) DEFAULT NULL,
                            PRIMARY KEY (`id`),
                            KEY `FKbnwj6i7md9xd8vr1pfqqbf1q5` (`member_id`),
-                           CONSTRAINT `FKbnwj6i7md9xd8vr1pfqqbf1q5` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+                           CONSTRAINT `FKbnwj6i7md9xd8vr1pfqqbf1q5` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -75,8 +94,8 @@ CREATE TABLE `preference` (
                               PRIMARY KEY (`id`),
                               KEY `FKrbs9ljrkj517ayouph71vol6n` (`category_id`),
                               KEY `FKg1k85wg4bngr1y82ahkbyyj2y` (`member_id`),
-                              CONSTRAINT `FKg1k85wg4bngr1y82ahkbyyj2y` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`),
-                              CONSTRAINT `FKrbs9ljrkj517ayouph71vol6n` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`)
+                              CONSTRAINT `FKg1k85wg4bngr1y82ahkbyyj2y` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE,
+                              CONSTRAINT `FKrbs9ljrkj517ayouph71vol6n` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -98,8 +117,8 @@ CREATE TABLE `recipe` (
                           PRIMARY KEY (`id`),
                           KEY `FKrufhnv33hpfxstx9x108553kj` (`category_id`),
                           KEY `FKsoo4pxu79yt7nethdmbiks3pe` (`member_id`),
-                          CONSTRAINT `FKrufhnv33hpfxstx9x108553kj` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`),
-                          CONSTRAINT `FKsoo4pxu79yt7nethdmbiks3pe` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+                          CONSTRAINT `FKrufhnv33hpfxstx9x108553kj` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE,
+                          CONSTRAINT `FKsoo4pxu79yt7nethdmbiks3pe` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -113,8 +132,8 @@ CREATE TABLE `collection` (
                               PRIMARY KEY (`id`),
                               KEY `FK7bm2ngjfpn2yrlhf90safc33l` (`member_id`),
                               KEY `FKj9v3rjhqhx0amf9fsj9eqaowh` (`recipe_id`),
-                              CONSTRAINT `FK7bm2ngjfpn2yrlhf90safc33l` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`),
-                              CONSTRAINT `FKj9v3rjhqhx0amf9fsj9eqaowh` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`)
+                              CONSTRAINT `FK7bm2ngjfpn2yrlhf90safc33l` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE,
+                              CONSTRAINT `FKj9v3rjhqhx0amf9fsj9eqaowh` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -134,8 +153,8 @@ CREATE TABLE `post` (
                         PRIMARY KEY (`id`),
                         KEY `FK83s99f4kx8oiqm3ro0sasmpww` (`member_id`),
                         KEY `FKmngn1r3090p14pnhhae23c8jr` (`recipe_id`),
-                        CONSTRAINT `FK83s99f4kx8oiqm3ro0sasmpww` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`),
-                        CONSTRAINT `FKmngn1r3090p14pnhhae23c8jr` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`)
+                        CONSTRAINT `FK83s99f4kx8oiqm3ro0sasmpww` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE,
+                        CONSTRAINT `FKmngn1r3090p14pnhhae23c8jr` FOREIGN KEY (`recipe_id`) REFERENCES `recipe` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -162,7 +181,7 @@ CREATE TABLE `reaction` (
                             KEY `FKf0kgc48u5e6wakvieex07kk5w` (`member_id`),
                             KEY `FKathfhl7fif9f9mggdjhg7ktdt` (`post_id`),
                             CONSTRAINT `FKathfhl7fif9f9mggdjhg7ktdt` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE,
-                            CONSTRAINT `FKf0kgc48u5e6wakvieex07kk5w` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+                            CONSTRAINT `FKf0kgc48u5e6wakvieex07kk5w` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -177,7 +196,7 @@ CREATE TABLE `scrap` (
                          KEY `FKq0ff1jblgu8vrsh90u826qell` (`member_id`),
                          KEY `FK6cjkagewjywuylil3gw7di68q` (`post_id`),
                          CONSTRAINT `FK6cjkagewjywuylil3gw7di68q` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE,
-                         CONSTRAINT `FKq0ff1jblgu8vrsh90u826qell` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+                         CONSTRAINT `FKq0ff1jblgu8vrsh90u826qell` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
@@ -194,8 +213,8 @@ CREATE TABLE `comment` (
                            KEY `FKmrrrpi513ssu63i2783jyiv9m` (`member_id`),
                            KEY `FKbpa02r5cfhqseda598fc5aqa5` (`parents_id`),
                            KEY `FKs1slvnkuemjsq2kj4h3vhx7i1` (`post_id`),
-                           CONSTRAINT `FKbpa02r5cfhqseda598fc5aqa5` FOREIGN KEY (`parents_id`) REFERENCES `comment` (`id`),
-                           CONSTRAINT `FKmrrrpi513ssu63i2783jyiv9m` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`),
+                           CONSTRAINT `FKbpa02r5cfhqseda598fc5aqa5` FOREIGN KEY (`parents_id`) REFERENCES `comment` (`id`) ON DELETE CASCADE,
+                           CONSTRAINT `FKmrrrpi513ssu63i2783jyiv9m` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE,
                            CONSTRAINT `FKs1slvnkuemjsq2kj4h3vhx7i1` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -211,5 +230,5 @@ CREATE TABLE `evaluation` (
                               KEY `FKb9ixvns6joh5jp42jfe9999nj` (`member_id`),
                               KEY `FK4nhl13aw1kvxdm2w4sb2nvefc` (`post_id`),
                               CONSTRAINT `FK4nhl13aw1kvxdm2w4sb2nvefc` FOREIGN KEY (`post_id`) REFERENCES `post` (`id`) ON DELETE CASCADE,
-                              CONSTRAINT `FKb9ixvns6joh5jp42jfe9999nj` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`)
+                              CONSTRAINT `FKb9ixvns6joh5jp42jfe9999nj` FOREIGN KEY (`member_id`) REFERENCES `member` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
