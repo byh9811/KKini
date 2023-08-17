79679869a4cab013cacc427be5fe979d883818dd
진병욱 <quddnr678@naver.com>
2023-08-17 17:47:35 +0900

    feat: 알고리즘, 레시피 무한 스크롤 구현

--- frontend/kkini/src/components/search/RecommendedFeed.jsx
+++ frontend/kkini/src/components/search/RecommendedFeed.jsx
@@ -1,16 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import "../../css/recipe.css";
 import FeedModal from "./../feed/FeedModal";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import ThumbDownOffAltRoundedIcon from "@mui/icons-material/ThumbDownOffAltRounded";
 import Loading from "../../routes/pages/Loading";
+import { useInView } from "react-intersection-observer";
 
 const RecommendedFeed = () => {
   const [data, setData] = useState([]);
   const [selectedPost, setSelectedPost] = useState(null);
   const [showModal, setShowModal] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [recipeLoading, setRecipeLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [hasMore, setHasMore] = useState(true);
+  const [ref, inView] = useInView();
+
+  const getAlgorithm = useCallback(async () => {
+    setRecipeLoading(true);
+    setLoading(true);
+    try {
+      const response = await axios.get("/post/algorithm", {
+        params: {
+          page: 0,
+        },
+      });
+      if (response.data.response.content.length > 0) {
+        setData((prevState) => [...prevState, ...response.data.response.content]);
+        setPage((prevState) => prevState + 1);
+      } else {
+        setHasMore(false);
+      }
+    } catch (error) {
+      console.error("추천 알고리즘 가져오기 오류 : " + error);
+    }
+    setRecipeLoading(false);
+    setLoading(false);
+  }, [page]);
 
   const handlePostClick = (id) => {
     setSelectedPost(id);
@@ -23,20 +50,13 @@ const RecommendedFeed = () => {
   };
 
   useEffect(() => {
-    setLoading(true);
-    axios
-      .get("/post/algorithm", {
-        params: {
-          page: 0,
-        },
-      })
-      .then((response) => {
-        setData(response.data.response.content);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    if (inView && !recipeLoading && hasMore) {
+      getAlgorithm();
+    }
+  }, [inView, recipeLoading, hasMore]);
+
+  useEffect(() => {
+    getAlgorithm();
   }, []);
 
   if (loading) {
@@ -45,19 +65,21 @@ const RecommendedFeed = () => {
 
   return (
     <div className="recipes-grid">
-      {data.map((item) => (
-        <div key={item.id} className="recipe-item">
-          <img src={item.imageList[0]} alt={`Image ${item.id}`} onClick={() => handlePostClick(item)} />
-          <div className="recipe-overlay">
-            <div>
-              <FavoriteBorderIcon /> {item.likeCnt}
+      {data.map((item, index) => (
+        <React.Fragment key={index}>
+          <div key={item.id} className="recipe-item" ref={index === data.length - 1 ? ref : null}>
+            <img src={item.imageList[0]} alt={`Image ${item.id}`} onClick={() => handlePostClick(item)} />
+            <div className="recipe-overlay">
+              <div>
+                <FavoriteBorderIcon /> {item.likeCnt}
+              </div>
+              <div>
+                <ThumbDownOffAltRoundedIcon /> {item.disLikeCnt}
+              </div>
+              <div>{item.memberName}</div>
             </div>
-            <div>
-              <ThumbDownOffAltRoundedIcon /> {item.disLikeCnt}
-            </div>
-            <div>{item.memberName}</div>
           </div>
-        </div>
+        </React.Fragment>
       ))}
       {selectedPost !== null && <FeedModal selectedPost={selectedPost} handleClose={handleCloseModal} show={showModal} />}
     </div>
