46a87bf9063ca2fb1ce707355cfb789152d66ffe
진병욱 <quddnr678@naver.com>
2023-08-17 16:57:22 +0900

    fix: 오류 수정 및 배경 테두리 색상 변경

--- frontend/kkini/src/routes/pages/Intro.jsx
+++ frontend/kkini/src/routes/pages/Intro.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../../css/intro.css";
 
-export default function Loading() {
+export default function Intro() {
   return (
     <div style={{ marginTop: "200px" }}>
       <img className="starWars logo my-auto mx-auto" src="img/logo2.png" alt="로고" />
