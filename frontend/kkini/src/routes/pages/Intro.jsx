1d960370bedbfaaa451599406bf8bea1f21d3433
김승영 <very7292@naver.com>
2023-08-17 14:54:20 +0900

    faet: 인트로

--- frontend/kkini/src/routes/pages/Intro.jsx
+++ frontend/kkini/src/routes/pages/Intro.jsx
@@ -1,11 +1,12 @@
-import React from "react";
-import "../../css/loading.css";
+import React from 'react';
+import '../../css/intro.css';
 
-export default function Loading() {
-  return (
-    <div class="loading-container">
-      <div class="loading"></div>
-      <div id="loading-text">loading</div>
-    </div>
-  );
+export default function Loading(){
+
+    return(
+        <div style={{ marginTop: '400px' }}>
+            <img className="spin3D logo my-auto mx-auto" src="img/logo2.png" alt="로고"/>
+        </div>
+    )
+
 }
